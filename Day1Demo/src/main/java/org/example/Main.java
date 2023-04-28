11cb55b195c2108fb3a5c5a90b378ae0bf1dc4d2
christina-snyder-kenzie <christina.snyder@kenzie.academy>
2023-04-28 12:45:45 -0400

    Git command Updates

---
+++ Day1Demo/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello and Welcome!");
+        for (int i = 1 ; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+
+
+        System.out.print("Hi It's Christina");
+    }
+}
